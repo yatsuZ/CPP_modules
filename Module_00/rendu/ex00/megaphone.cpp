f49951cff19644b1d32d2f60efd521557118cbdb
yatsuZ <y.zaoui.pro@gmail.com>
2024-03-26 18:51:13 +0100

    AU final je vais re apprendre les makfile

--- Module_00/rendu/ex00/megaphone.cpp
+++ Module_00/rendu/ex00/megaphone.cpp
@@ -6,7 +6,17 @@
 /*   By: yzaoui <yzaoui@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 22:08:55 by yzaoui            #+#    #+#             */
-/*   Updated: 2024/03/18 22:08:56 by yzaoui           ###   ########.fr       */
+/*   Updated: 2024/03/26 17:07:11 by yzaoui           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	(void)	argc;
+	(void)	argv;
+	std::cout << "HELLO WORLD" << std::endl;
+	return (0);
+}
