e277cf601035a47d0b16394c293bf29f9725af08
YatsuZ <yassine.zaoui28062003@gmail.com>
2024-04-08 02:33:16 +0200

    Correction du nommage de mes prog

---
+++ Module_03/rendu/ex00/ClapTrap.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yzaoui <yzaoui@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 02:25:30 by yzaoui            #+#    #+#             */
+/*   Updated: 2024/04/08 02:31:32 by yzaoui           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+
+#define NOCOLOR "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+#define PURPLE "\033[35m"
+
+#endif
